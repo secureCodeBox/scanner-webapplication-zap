d3dcbfb52748c5ed275841d83fcc9eb2ed4dc990
Robert Seedorff <Robert.Seedorff@iteratec.de>
2018-05-16 23:20:02 +0200

    Trying to fix missing home directory (open shift)

--- dockerfiles/init.sh
+++ dockerfiles/init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-/zap/zap.sh -daemon -port 8090 -host 0.0.0.0 -config api.disablekey=true -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true -addoninstall soap -addoninstall openapi &
+export ZAP_PATH /zap/zap.sh
+export HOME /home/zap/
+
+/zap/zap.sh -daemon -dir /home/zap/ -port 8090 -host 0.0.0.0 -config api.disablekey=true -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true -addoninstall soap -addoninstall openapi &
 
 sleep 5
 
