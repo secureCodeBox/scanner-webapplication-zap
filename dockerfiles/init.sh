e9baefd2420e7bfcdf62e4258e7195b17d764477
Timo Pagel <github@timo-pagel.de>
2018-11-29 15:05:19 +0100

    Add alpha and beta active/passiv scan rules

--- dockerfiles/init.sh
+++ dockerfiles/init.sh
@@ -7,7 +7,7 @@ if [ `id -u` -ge 1000 ]; then
     rm /tmp/passwd
 fi
 
-/zap/zap.sh -Xmx3G -daemon -dir /home/zap/ -port 8090 -host 0.0.0.0 -config api.disablekey=true -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true -addoninstall soap -addoninstall openapi &
+/zap/zap.sh -Xmx3G -daemon -dir /home/zap/ -port 8090 -host 0.0.0.0 -config api.disablekey=true -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true -addoninstall soap -addoninstall openapi -addoninstall ascanrulesBeta -addoninstall ascanrulesAlpha -addoninstall pscanrulesBeta &
 
 sleep 10
 
